315fbf17e43b1f7865ed42e678fb86382aeaad1b
Marian Vennewald <vennewald@axxessio.com>
2016-12-08 16:29:46 +0100

    config acctionmail for gmail

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-ActionMailer::Base.smtp_settings = {
-    :user_name => 'admin',
-    :password => 'admin1234',
-    :domain => 'vkm.marianit.de',
-    :address => 'smtp.marianit.de',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
