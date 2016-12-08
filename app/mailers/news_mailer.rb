3d3d1ab932a4ef1b94e9fa708901a865ab3b885d
Marian Vennewald <vennewald@axxessio.com>
2016-12-08 15:48:36 +0100

    send email after newsletter is created, fix

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -6,8 +6,8 @@ class NewsMailer < ActionMMailer::Base
     @aboNewsletter.each do |aboNewsletter|
       if(aboNewsletter.enable == true)
         @email = aboNewsletter.eMail
-        mail (to: @email,
-          subject: "News-Mail")
+        mail to: @email,
+          subject: "News-Mail"
       end
     end
 
