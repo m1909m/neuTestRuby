57b9f815c50a77a7abc4311f9e3faa64c6397f55
Marian Vennewald <vennewald@axxessio.com>
2016-12-08 15:03:12 +0100

    Maybe fix problem with button rout to send emaail

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'current/index'
   root :to => "current#index"
 
+  get "newsletters/sendNewsletter" => "newsletters#sendNewsletter"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
