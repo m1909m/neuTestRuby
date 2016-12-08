14fc3cfaa54c0d6ca6f8cd4a10b1b980bcc87e0d
Marian Vennewald <vennewald@axxessio.com>
2016-12-08 16:39:07 +0100

    config acctionmail for gmail teil2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,14 +84,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "example.com",
+      user_name: ENV['gmail_username'],
+      password: ENV['gmail_password'],
+      domain: "vkm.marianit.de",
       authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "verbandKirchlicherMitarbeiter",
-      password: ".bEaQd.3BX"
+      enable_starttls_auto: true
   }
 
 end
