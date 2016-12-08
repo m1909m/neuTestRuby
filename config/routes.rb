c33d83a3a30446056d0f38ee5c7e5bf717788935
Marian Vennewald <vennewald@axxessio.com>
2016-12-08 21:10:35 +0100

    edit layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'current/index'
   root :to => "current#index"
 
-  get '/abo_newsletter/:email', to: "AboNewsletters#signOutNews"
+  get '/abo_newsletter/:email', to: "abonewsletters#signOutNews"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
