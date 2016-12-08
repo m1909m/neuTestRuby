b6c0ade6637b8fc07fe24f5107632e36d30bc716
Marian Vennewald <vennewald@axxessio.com>
2016-12-08 17:33:27 +0100

    Test config develepmont

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address=> "smtp.gmail.com",
       :port=> 587,
-      :user_name=> ENV['gmail_username'],
-      :password=> ENV['gmail_password'],
+      :user_name=> 'verbandkirchlichermmitarbeiter@gmail.com',
+      :password=> '.bEaQd.3BX',
       :domain=> "vkm.marianit.de",
       :authentication=> "plain",
       :enable_starttls_auto=> true
