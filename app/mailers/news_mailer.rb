16427875fa5f27a5cd9f31e23b7361b60cbb0619
Marian Vennewald <vennewald@axxessio.com>
2016-12-09 00:28:26 +0100

    Newsletter abmelden

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -6,6 +6,7 @@ class NewsMailer < ActionMailer::Base
 
       if(@aboNewsletters.enable == true)
         @email = @aboNewsletters.eMail
+        @url = "http://vkm.marianit.de/abo_newsletter/" + @aboNewsletters.eMail
         mail :to=> @email,
              :subject=> "News-Mail"
       end
