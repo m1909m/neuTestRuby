db60698bffa249e3007ce8cafbea8d390cbd0ea5
Marian Vennewald <vennewald@axxessio.com>
2016-12-08 19:08:30 +0100

    add tinymce

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,6 +1,6 @@
 class Newsletter < ApplicationRecord
   #has_many :abo_newsletters
   #Prüfung auf vorhandensein
-  validates :from, :to, :newsHeader, :newsBody, :newsFooter, presence: true
+  validates :from, :newsHeader, :newsBody, :newsFooter, presence: true
 
 end
