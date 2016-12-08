5de4ff9ca7e62f13599914906235c63df2a78988
Marian Vennewald <vennewald@axxessio.com>
2016-12-09 00:14:38 +0100

    Newsletter abmelden

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,14 +1,12 @@
 class NewsMailer < ActionMailer::Base
-  default from: 'marian.vennewald@gds-medien.de'
+  default from: 'info@vkm.de'
   def news_email(newsletter, abo_newsletter)
     @aboNewsletters = abo_newsletter
     @newsletter = newsletter
 
       if(@aboNewsletters.enable == true)
-        @fromV = @newsletter.from
         @email = @aboNewsletters.eMail
-        mail :from=> @fromV,
-             :to=> @email,
+        mail :to=> @email,
              :subject=> "News-Mail"
       end
 
