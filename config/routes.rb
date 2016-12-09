e3288156b8cdf72dcdc7652dea3ea61c97a711e2
Marian Vennewald <vennewald@axxessio.com>
2016-12-09 11:31:41 +0100

    First sign in

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   resources :abo_newsletters
   resources :newsletters
   get 'current/index'
-  if current_user.present?
+
   root :to => "newssystems#index"
-  else
-    root :to => "devise/sessions#new"
-  end
+
   get '/abo_newsletter/:email', to: "abo_newsletters#signOutNews"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
