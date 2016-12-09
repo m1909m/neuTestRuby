c76a52b553bfd5fbc1122449fa42106bd20992e2
m1909m <m-vennewald@web.de>
2016-12-09 10:52:09 +0100

    rout / to newssystem. index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :abo_newsletters
   resources :newsletters
   get 'current/index'
-  root :to => "current#index"
+  root :to => "newssystems#index"
 
   get '/abo_newsletter/:email', to: "abo_newsletters#signOutNews"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
