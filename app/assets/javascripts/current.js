27d31d4f06d103aeb9a33d6224e0819436923ad3
Marian Vennewald <vennewald@axxessio.com>
2017-02-09 23:45:23 +0100

    remove materialdesign angular

--- app/assets/javascripts/current.js
+++ app/assets/javascripts/current.js
@@ -2,7 +2,7 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 var app = angular
-  .module('app', ['ngMaterial', 'ui.calendar'])
+  .module('app', ['ui.calendar'])
   .controller('appController', ['$scope', function($scope) {
 
 $calendar = $('[ui-calendar]');
