1580681ae763aa5f8ad0c4dcf5501e1352be553f
Marian Vennewald <vennewald@axxessio.com>
2017-02-10 08:21:02 +0100

    change save date to startDate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :c_events
-    resource :rooms
+  resources :rooms
 
   resources :roomplanings
   ActiveAdmin.routes(self)
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get 'administrator/rooms', to: "rooms#angular"
 
-  get '/rooms', to: "rooms#index"
+  #get '/rooms', to: "rooms#index"
 
   get '/abo_newsletter/:email', to: "abo_newsletters#signOutNews"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
