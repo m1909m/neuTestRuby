3af42ef9c7c94f3f4838a10a15e8821e0323155b
Marian Vennewald <vennewald@axxessio.com>
2017-02-12 21:28:01 +0100

    fas

--- app/models/c_event.rb
+++ app/models/c_event.rb
@@ -1,4 +1,8 @@
 class CEvent < ApplicationRecord
   has_one :room
   accepts_nested_attributes_for :room
+
+  def all_day_event?
+    self.start == self.start.midnight && self.end == self.end.midnight ? true : false
+  end
 end
