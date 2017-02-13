8c3a4df30779f0f09146cd2d76cb0906a57d7036
Marian Vennewald <vennewald@axxessio.com>
2017-02-13 04:19:07 +0100

    sdf

--- app/models/c_event.rb
+++ app/models/c_event.rb
@@ -3,6 +3,10 @@ class CEvent < ApplicationRecord
   accepts_nested_attributes_for :room
   belongs_to :member
 
+  def name_with_initial
+    "#{title}"
+  end
+
   def all_day_event?
     self.start == self.start.midnight && self.end == self.end.midnight ? true : false
   end
