959d04291d1c91149cb9c14dba33c20cf6ad73e6
Marian Vennewald <vennewald@axxessio.com>
2017-02-13 07:00:59 +0100

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :members
+
   resources :c_events,only: [:index, :create, :destroy], defaults: {format: :json}
   resources :c_events,only: [:new]
   resources :rooms, defaults: {format: :json}
@@ -12,12 +13,15 @@ Rails.application.routes.draw do
   resources :abo_newsletters
   resources :newsletters
   get 'current/index'
+  get 'verband', to "current#verband"
 
   root :to => "newssystems#index"
 
   get 'administrator/rooms', to: "rooms#angular"
 
   #get '/rooms', to: "rooms#index"
+  get 'members/new/:id', to: "members#new"
+
 
   get '/abo_newsletter/:email', to: "abo_newsletters#signOutNews"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
