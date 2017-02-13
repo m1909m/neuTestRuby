c6c0aa94244e2c3e319ad45db410a84a9c894fdf
Marian Vennewald <vennewald@axxessio.com>
2017-02-13 14:06:00 +0100

    fas

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,7 +6,7 @@ class MembersController < ApplicationController
   # GET /members.json
   def index
     @members = Member.all
-    @events = CEvent.where(free: true).where('startLogin <= ? AND endLogin >= ?', Date.today.strftime("%Y-%m-%dT%H:%M:%S%Z"), Date.today.strftime("%Y-%m-%dT%H:%M:%S%Z"))
+    @events = CEvent.where(free: true).where(startLogin < Date.current.strftime("%Y-%m-%dT%H:%M:%S%Z")).where(endLogin > Date.current.strftime("%Y-%m-%dT%H:%M:%S%Z"))
     @rooms = Room.all
   end
 
