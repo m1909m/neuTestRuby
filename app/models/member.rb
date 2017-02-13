8b9f972ae4b461ffaae34ef5687fa767f97a4e13
Marian Vennewald <vennewald@axxessio.com>
2017-02-13 11:12:42 +0100

    fas

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,11 @@
 class Member < ApplicationRecord
-  belongs_to :c_event
+  has_one :c_event
 
   accepts_nested_attributes_for :c_event
   after_create :increment_member
 
   def increment_member
-    self.c_event.update_attribute(:member, self.c_event.member)
+    c_event.member = c_event.member + 1
+
   end
 end
