4187f29f0dc0de2d91434076de593ec5030ad3da
Marian Vennewald <vennewald@axxessio.com>
2017-02-13 11:36:51 +0100

    fassdfsf

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -104,11 +104,11 @@ myApp.controller('eventController', ['$scope', '$stateParams', 'Room', function(
 
     $scope.events = [];
     $scope.events = Room.events.index({"roomid": $stateParams.id});
-    rooms = Room.rooms.index();
-    for(i = 0;i++;i<rooms.length) {
+    room = Room.rooms.index($stateParams.id);
+    /*for(i = 0;i++;i<rooms.length) {
         if(rooms[i].id == $stateParams.id)
             $scope.room = rooms[i];
-    }
+    }*/
     $scope.addEvent = function() {
 
         newEvent = $scope.newEvent;
