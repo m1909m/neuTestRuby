22beebc2fd0219d5649a89979f71a4cf384f2800
Marian Vennewald <vennewald@axxessio.com>
2017-02-13 10:55:22 +0100

    asf

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,8 @@ class Member < ApplicationRecord
 
   accepts_nested_attributes_for :c_event
   after_create :increment_member
-  def increment_counter
+
+  def increment_member
     self.c_event.update_attribute(:member, self.c_event.member + 1)
   end
 end
