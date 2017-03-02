905c3ad497096491c2d553ec92130eec5010fd7d
Marian Vennewald <vennewald@axxessio.com>
2017-03-02 14:21:09 +0100

    maybe fix error @contact_forms is null

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -7,13 +7,15 @@ class ContactFormsController < ApplicationController
   def create
     begin
       @contact_form = ContactForm.new(contactForms_params)
-      @contact_form.request = request
-      if @contact_form.deliver
-        if @contact_form.save
-          flash.now[:notice] = 'Danke für Ihre Nachricht!'
+      #@contact_form.request = request
+      if @contact_form.save
+        if ContactMailer.contact_email(@contact_form).deliver
+
+            flash.now[:notice] = 'Danke für Ihre Nachricht!'
+
+        else
+          render :new
         end
-      else
-        render :new
       end
     rescue ScriptError
       flash[:error] = 'Schuldige, Nachricht wurde nicht versendet.'
