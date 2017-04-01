1fb2b0611266b5984577fd5800046d946aa6dbb6
Marian Vennewald <vennewald@axxessio.com>
2017-04-01 20:01:15 +0200

    edit formular for formular and tinymce

--- app/admin/newsletter.rb
+++ app/admin/newsletter.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Newsletter do
 
+  permit_params :newsHeader, :newsBody, :newsFooter
+
+  form do |f|
+    f.inputs 'Newsletter layout erstellen' do
+      f.input :newsHeader, :html => { :class => "tinymce" }
+      f.input :newsBody
+      f.input :newsFooter
+    end
+    f.actions
+  end
 
 end
