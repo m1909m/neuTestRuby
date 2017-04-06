57c06699b4d52e9781c53493d04d4b47ac185b76
Marian Vennewald <vennewald@axxessio.com>
2017-04-06 16:04:30 +0200

    add some js for modal

--- app/assets/javascripts/current.js
+++ app/assets/javascripts/current.js
@@ -2,6 +2,10 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
+
 var app = angular
   .module('app', ['ui.calendar'])
   .controller('appController', ['$scope', function($scope) {
