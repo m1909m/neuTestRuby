8fb1c96e9bdfcb274a3ba56994fc4ad22cc4b25b
Marian Vennewald <vennewald@axxessio.com>
2017-04-07 13:27:27 +0200

    add angularforwarenkorb

---
+++ app/assets/javascripts/shops.js
@@ -0,0 +1,17 @@
+var shopApp = angular.module('shopping', [ 'ui.router', 'ngResource' ]);
+
+shopApp.factory("Cart", function($ressource) {
+    var o = { };
+    o.articles = [
+        {"id": "1", "title": "Buch 1", "price": 5 },
+        {"id": "2", "title": "Buch 2", "price": 5.5 },
+        {"id": "3", "title": "Buch 3", "price": 6 },
+        {"id": "4", "title": "Buch 4", "price": 0 }
+    ];
+    return o;
+});
+
+shopApp.controller('cartCotroller', ['$scope', 'Cart','$http', '$interval', function($scope, Cart, $http, $interval) {
+    $scope.articles = Cart.articles;
+
+}]);
---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,18 @@
+class ShopController < ActionController
+
+  def batkf
+
+  end
+
+  #def entgelttabellen
+
+  #end
+
+  def mvg
+
+  end
+
+  def handreichungen
+
+  end
+end
