2a42e7cfef8682a7cf438257ab2df30fea15b8ce
Marian Vennewald <vennewald@axxessio.com>
2017-04-08 13:41:26 +0200

    add rootscope in controllers

--- app/assets/javascripts/shops.js
+++ app/assets/javascripts/shops.js
@@ -1,35 +1,8 @@
 'use strict';
 
-var shopApp = angular.module('shopping', [ 'ui.router' ], function($provide) {
-    $provide.factory("Cart", ['$rootscope', function($rootscope) {
-        var o = { };
-        $rootscope.items = {};
-        o.items = [];
-        o.addItems = function(article) {
-            $rootscope.items.push(article);
-        };
-        o.getItems = function() {
-            return $rootscope.items;
-        };
-        /*return {
-         getItems: function() {
-         return items;
-         },
-         addArticle: function(article) {
-         items.push(article);
-         },
-         sum: function() {
-         return items.reduce(function(total, article) {
-         return total + article.price;
-         }, 0);
-         }
-         //        {"id": "1", "title": "Buch 1", "price": 5 }
-         };*/
-        return o;
-    }]);
-});
+var shopApp = angular.module('shopping', [ 'ui.router' ]);
 
-/*
+
 shopApp.factory("Cart", ['$rootscope', function($rootscope) {
 
     var o = { };
@@ -54,11 +27,11 @@ shopApp.factory("Cart", ['$rootscope', function($rootscope) {
             }, 0);
         }
 //        {"id": "1", "title": "Buch 1", "price": 5 }
-    };
+    };*/
     return o;
 }]);
-*/
-shopApp.controller('shopController', ['$scope', 'Cart','$http', '$interval', function($scope, Cart, $http, $interval) {
+
+shopApp.controller('shopController', ['$scope' , '$rootscope', 'Cart','$http', '$interval', function($scope, $rootscope, Cart, $http, $interval) {
     $scope.articles = [];
     $scope.articles = [
         {"id": "1", "name": "Pizza Vegetaria", "price": 5 },
@@ -66,15 +39,16 @@ shopApp.controller('shopController', ['$scope', 'Cart','$http', '$interval', fun
         {"id": "3", "name": "Pizza Thunfisch", "price": 6 },
         {"id": "4", "name": "Aktueller Flyer", "price": 0 }
     ];
-    Cart.addItems({"id": "1", "name": "Test 1", "price": 5 });
-    $scope.cart = Cart.getItems();
+    $rootscope.items = {};
+    $rootscope.items.push({"id": "1", "name": "Test 1", "price": 5 });
+    $scope.cart = $rootscope.items;
 
 
 }]);
 
-shopApp.controller('cartController', ['$scope', 'Cart','$http', '$interval', function($scope, Cart, $http, $interval) {
+shopApp.controller('cartController', ['$scope', '$rootscope', 'Cart','$http', '$interval', function($scope, $rootscope, Cart, $http, $interval) {
 
     $scope.cart = {};
-    $scope.cart = Cart.getItems();
+    $scope.cart = $rootscope.items;
 
 }]);
