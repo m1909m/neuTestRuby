6bfd41144930990cf4cba89b196caa60e20bbe0f
Marian Vennewald <vennewald@axxessio.com>
2017-04-08 13:53:15 +0200

    test

--- app/assets/javascripts/shops.js
+++ app/assets/javascripts/shops.js
@@ -1,9 +1,35 @@
 'use strict';
+angular.moule('myModule', []).factory("Cart", function() {
 
+    var o = { };
+    $rootscope.items = {};
+    o.items = [];
+    o.addItems = function(article) {
+        $rootscope.items.push(article);
+    };
+    o.getItems = function() {
+        return $rootscope.items;
+    };
+    /*return {
+     getItems: function() {
+     return items;
+     },
+     addArticle: function(article) {
+     items.push(article);
+     },
+     sum: function() {
+     return items.reduce(function(total, article) {
+     return total + article.price;
+     }, 0);
+     }
+     //        {"id": "1", "title": "Buch 1", "price": 5 }
+     };*/
+    return o;
+});
 var shopApp = angular.module('shopping', [ 'ui.router' ]);
 
 
-shopApp.controller('shopController', ['$scope' , '$rootscope', 'Cart','$http', '$interval', function($scope, $rootscope, Cart, $http, $interval) {
+shopApp.controller('shopController', ['$scope' , 'Cart','$http', '$interval', function($scope, Cart, $http, $interval) {
     $scope.articles = [];
     $scope.articles = [
         {"id": "1", "name": "Pizza Vegetaria", "price": 5 },
@@ -11,16 +37,16 @@ shopApp.controller('shopController', ['$scope' , '$rootscope', 'Cart','$http', '
         {"id": "3", "name": "Pizza Thunfisch", "price": 6 },
         {"id": "4", "name": "Aktueller Flyer", "price": 0 }
     ];
-    $rootscope.items = {};
-    $rootscope.items.push({"id": "1", "name": "Test 1", "price": 5 });
-    $scope.cart = $rootscope.items;
+    $rootscope.cart = {};
+    Cart.addItems({"id": "1", "name": "Test 1", "price": 5 });
+    $scope.cart = Cart.getItems();
 
 
 }]);
 
-shopApp.controller('cartController', ['$scope', '$rootscope', 'Cart','$http', '$interval', function($scope, $rootscope, Cart, $http, $interval) {
+shopApp.controller('cartController', ['$scope', 'Cart','$http', '$interval', function($scope, Cart, $http, $interval) {
 
     $scope.cart = {};
-    $scope.cart = $rootscope.items;
+    $scope.cart = Cart.getItems();
 
 }]);
