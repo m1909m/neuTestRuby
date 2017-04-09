0f2f6f4af18c09552e818de16842c3d5e78bdd52
Marian Vennewald <vennewald@axxessio.com>
2017-04-09 22:39:52 +0200

    add

--- db/migrate/20170408165055_change_articles.rb
+++ db/migrate/20170408165055_change_articles.rb
@@ -1,5 +1,5 @@
 class ChangeArticles < ActiveRecord::Migration[5.0]
   def change
-    change_column :cards, :articles, :text, default: [], array:true, using: "(string_to_array(articles, ','))"
+    change_column :cards, :articles, :text, default: [], array:true
   end
 end
