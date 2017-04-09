2b17de201e4105e55e62ec044d734f50990f1fcf
Marian Vennewald <vennewald@axxessio.com>
2017-04-09 23:44:03 +0200

    add

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -89,6 +89,6 @@ class CardsController < ApplicationController
   end
 
   def article_params
-    params.require(:card).permit(:articles)
+    params.require(:articles).permit(:articles)
   end
 end
