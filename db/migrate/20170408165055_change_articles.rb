7a99b8fda44971cfb3c921d94d2d411a362eb453
Marian Vennewald <vennewald@axxessio.com>
2017-04-09 22:37:09 +0200

    test without array

--- db/migrate/20170408165055_change_articles.rb
+++ db/migrate/20170408165055_change_articles.rb
@@ -1,5 +1,5 @@
 class ChangeArticles < ActiveRecord::Migration[5.0]
   def change
-    change_column :cards, :articles, :text, array: true, default: [], using: "(string_to_array(articles, ','))"
+    change_column :cards, :articles, :text, default: [], using: "(string_to_array(articles, ','))"
   end
 end
