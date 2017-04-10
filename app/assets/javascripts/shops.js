79e45231ec742a29254e4e9db6384d18c1c1561a
Marian Vennewald <vennewald@axxessio.com>
2017-04-10 21:56:14 +0200

    test

--- app/assets/javascripts/shops.js
+++ app/assets/javascripts/shops.js
@@ -1,13 +1,16 @@
 'use strict';
 angular.module('myModule', ['ngStorage', 'ngResource']).factory("Cart", function($window, $rootScope, $resource) {
-
+    angular.element($window).on('storage', function(event) {
+        if (event.key === 'warenkorb') {
+            $rootScope.$apply();
+        }
+    });
     var o = { };
     o.cards = $resource("/cards/:id", { id: '@id' }, {
         index:   { method: 'GET', isArray: true, responseType: 'json' },
         update:  { method: 'PUT', responseType: 'json' }
     });
     o.items = [];
-    var count = 0;
 
     o.addItems = function(article) {
         var items = [];
@@ -17,7 +20,7 @@ angular.module('myModule', ['ngStorage', 'ngResource']).factory("Cart", function
         // TODO anzahl articel
         items.push(article);
         $window.sessionStorage.setItem("warenkorb", JSON.stringify(items));
-        o.items = items;
+        //o.items = items;
 
         //o.items.push(JSON.stringify(article));
     };
@@ -52,7 +55,7 @@ shopApp.run( ["$rootScope", function($rootScope) {
     $rootScope.articles = [];
 }]);
 
-shopApp.controller('shopController', ['$scope' , 'Cart', '$http', '$interval', function($scope, Cart, $http, $interval) {
+shopApp.controller('shopController', ['Cart', function(Cart) {
     /*
     $scope.articles = [
         {"id": "1", "name": "Pizza Vegetaria", "price": 5 },
@@ -60,21 +63,12 @@ shopApp.controller('shopController', ['$scope' , 'Cart', '$http', '$interval', f
         {"id": "3", "name": "Pizza Thunfisch", "price": 6 },
         {"id": "4", "name": "Aktueller Flyer", "price": 0 }
     ]; */
-    $scope.articles = Cart.cards.index({"site": "batkf"});
+    this.articles = Cart.cards.index({"site": "batkf"});
 
   //  Cart.addItems({"id": "1", "name": "Test 1", "price": 5 });
   //  $scope.cart = Cart.getItems();
-    $scope.addArticle = function(article) {
+    this.addArticle = function(article) {
         Cart.addItems(article);
-        /*
-        $http({
-            method: "GET",
-            url:"/cards/counterDown/{{article.id}}"
-        }).then( function (response) {
-
-        }, function error(response) {
-
-        });*/
     };
 
 
@@ -83,22 +77,20 @@ shopApp.controller('shopController', ['$scope' , 'Cart', '$http', '$interval', f
 
 // var cartApp = angular.module('cartApp', [ 'myModule', 'shopping', 'ui.router' ]);
 
-shopApp.controller('cartController', ['$scope', 'Cart','$http', '$interval', function($scope, Cart, $http, $interval) {
-
-
+shopApp.controller('cartController', ['Cart', function(Cart) {
 /*
     $scope.$watch(function () {
         $scope.cards = Cart.getItems();
     }, function () {
     });*/
 
-    $scope.card = Cart.getItems();
-    $scope.getCard = function() {
+    this.card = Cart.getItems();
+    this.getCard = function() {
         return Cart.getItems();
     };
   //  $scope.cart = Cart.getItems();
 
-    $scope.removeArticle = function(article) {
+    this.removeArticle = function(article) {
         var items = [];
         var articles = Cart.getItems();
         Cart.clearItem();
