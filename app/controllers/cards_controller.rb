7528e2f5e160efa5ec1046accfe92697b1551ccd
Marian Vennewald <vennewald@axxessio.com>
2017-04-10 04:43:07 +0200

    add

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -33,7 +33,7 @@ class CardsController < ApplicationController
   def create
     @card = Card.new
     @newPerson = Person.new
-    puts(params[:_json][:name])
+    puts(params[0][:name])
     @newPerson.name = params[:name]
     @newPerson.vorname = params[:vorname]
     @newPerson.einrichtung = params[:einrichtung]
