2730c16cfb482431804c64d1771ca7fe3705f7f8
Marian Vennewald <vennewald@axxessio.com>
2017-04-10 06:46:46 +0200

    test other article by admin

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,4 +1,22 @@
 ActiveAdmin.register Article do
+  permit_parmas :title, :price, :count, :site
 
+  index do
+    column "Artikel Titel", :title
+    column "Preis", :price
+    column "Verfügbar", :count
+    column "Für Seite:", :site
+    actions
+  end
+
+  form do |f|
+    f.inputs 'Neuen Artikel anlegen' do
+      f.input :title, "Artikel Titel"
+      f.input :price, "Preis pro Artikel"
+      f.input :count, "Anzahl an Artikel"
+      f.input :site, "Für Seite: "
+    end
+    f.actions
+  end
 
 end
