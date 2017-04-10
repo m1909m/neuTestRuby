c0cd5ac42391cf4e22f868841e361bf12360d098
Marian Vennewald <vennewald@axxessio.com>
2017-04-10 05:08:48 +0200

    rightsthgoughtstable

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ApplicationRecord
   belongs_to :person
 
   has_many :bookings
-  has_many :articles, through: :booking
+  has_many :articles, through: :bookings
 
   accepts_nested_attributes_for :person
   accepts_nested_attributes_for :articles
