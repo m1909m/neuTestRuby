b6c729729fe3dccb863637d4077c7ff528ebf793
Marian Vennewald <vennewald@axxessio.com>
2017-04-11 11:50:29 +0200

    add

--- app/controllers/newscontents_controller.rb
+++ app/controllers/newscontents_controller.rb
@@ -76,7 +76,7 @@ class NewscontentsController < ApplicationController
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_newsletter
+  def set_newscontent
     @newscontent = Newscontent.find(params[:id])
   end
 
