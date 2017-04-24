189b7c666b2bd936629301d76e35d9b0b667287d
Marian Vennewald <vennewald@axxessio.com>
2017-04-25 00:20:04 +0200

    add pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,24 @@
-class RoomsController < ApplicationController
+class PagesController < ApplicationController
 
-  respond_to :json
-
-  before_action :set_room, only: [:show, :edit, :update, :destroy]
-
-  def angular
-
-    #@rooms = Room.all
-    render 'rooms/index'
-  end
+  before_action :set_pages, only: [:index]
 
   # GET /rooms
   # GET /rooms.json
   def index
     # @rooms = Room.all
+    @site = params[:site]
+
     respond_to do |format|
-      format.json { render json: Room.all}
+      format.html
 
     end
   end
 
   def show
-    render 'rooms/show'
   end
 
   def new
-    @room = Room.new
+
   end
 
   def edit
@@ -37,19 +30,19 @@ class RoomsController < ApplicationController
   end
 
   def update
-    @room.update(room_params)
+    @page.update(room_params)
 
   end
 
   def destroy
-    @room.destroy
+    @page.destroy
 
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_room
-    @page = Page.find(params[:id])
+  def set_pages
+    @pages = Page.where(for: params[:site])
   end
   def room_params
     params.require(:page).permit(:content, :for, :free)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pages
 
   resources :news
   get '/admin/news', to: "news#admin"
