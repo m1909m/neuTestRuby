0df33f8ba91433d86c17e52e6f277934132c26cf
Marian Vennewald <vennewald@axxessio.com>
2017-04-24 10:01:08 +0200

    add controller

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -75,7 +75,7 @@ myApp.controller('RoomCtrl', ['$scope', 'Room', '$http', '$interval', function($
 }]);
 
 myApp.controller('calendarController', ['$scope', '$stateParams', 'Room', '$http', '$interval', function($scope, $stateParams, Room, $http, $interval) {
-    $scope.roomId = <%= @id %>;
+    $scope.roomId = '#{@id}';
     $scope.events = [];
     $scope.events = Room.events.index({"roomid": $scope.roomId });
     var date = new Date();
