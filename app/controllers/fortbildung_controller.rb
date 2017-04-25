a694b3efe9510b54d888456a1d3d2d77e28e7cdf
Marian Vennewald <vennewald@axxessio.com>
2017-04-25 14:44:56 +0200

    add controller

--- app/controllers/fortbildung_controller.rb
+++ app/controllers/fortbildung_controller.rb
@@ -1,6 +1,5 @@
 class FortbildungController < ApplicationController
   def fortbildung
-
   end
 
   def fachgruppen
@@ -8,12 +7,12 @@ class FortbildungController < ApplicationController
   end
 
   def seminarinhalte
-
+    @page = Page.where(accept: true, for: "Seminarinhalte").order(:id).last
   end
   def bedingungen
-
+    @page = Page.where(accept: true, for: "Teilnahmebedingungen").order(:id).last
   end
   def schulung
-
+    @page = Page.where(accept: true, for: "Schulung").order(:id).last
   end
 end
