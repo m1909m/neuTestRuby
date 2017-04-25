3974e933473ef35322c53b995bfdb21d12107d54
Marian Vennewald <vennewald@axxessio.com>
2017-04-25 15:01:26 +0200

    add last render from db

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,18 +1,19 @@
 class ShopController < ApplicationController
 
   def batkf
-    respond_to :html, :js
+    @page = Page.where(accept: true, for: "batkf").order(:id).last
   end
 
   def entgelttabellen
-
+    @page = Page.where(accept: true, for: "entgelttabellen").order(:id).last
   end
 
   def mvg
-
+    @page = Page.where(accept: true, for: "mvg").order(:id).last
   end
 
   def handreichungen
-
+    @page = Page.where(accept: true, for: "handreichungen").order(:id).last
   end
+
 end
