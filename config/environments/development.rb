4d3d6b6869347c98724d6b27f8acbd3a24ef2692
Marian Vennewald <vennewald@axxessio.com>
2017-04-30 15:34:22 +0200

    add config delaye_job

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
 
+  config.active_job.queue_adapter = :delayed_job
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
