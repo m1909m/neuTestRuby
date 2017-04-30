2d13315f6f1bb84094d2d8cd9e4eca5154a810e8
Marian Vennewald <vennewald@axxessio.com>
2017-04-30 14:50:14 +0200

    test mailing 20 second later

--- app/controllers/news_contents_controller.rb
+++ app/controllers/news_contents_controller.rb
@@ -34,7 +34,7 @@ class NewsContentsController < ApplicationController
 
     if @news_content.save
       @layout = Newsletter.find(@news_content.newsletter_id)
-      SendEmailJob.set(wait_until: @news_content.sendtime).perform_later(@layout, @news_content)
+      SendEmailJob.set(wait: 20.seconds).perform_later(@layout, @news_content)
       respond_to do |f|
         f.html { redirect_to news_contents_url, notice: 'News wurde erfolgreich erstellt'}
       end
