85f3ed582099298a84d0a02579333977e20d6c64
Marian Vennewald <vennewald@axxessio.com>
2017-04-30 16:11:33 +0200

    some changes

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -2,9 +2,17 @@ class SendEmailJob < ApplicationJob
   queue_as :default
 
   def perform(newsletter, newscontent)
+    @aboNewsletters = AboNewsletter.all
     @newsletter = newsletter
     @newscontent = newscontent
-    NewsMailer.news_email(@newsletter, @newscontent).deliver_later
+    @abo_newsletters.each do |abo_newsletter|
+      if(abo_newsletter.enable == true)
+        NewsMailer.news_email(@newsletter, @newscontent, abo_newsletter).deliver_later
     # Do something later
+      end
+    end
+    @news = NewsContent.find(@newscontent.id)
+    @news.sendStatus = true
+    @news.save
   end
 end
--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,21 +1,16 @@
 class NewsMailer < ActionMailer::Base
   default from: 'info@vkm.de'
-  def news_email(newsletter, newscontent)
-    @aboNewsletters = AboNewsletter.all
+  def news_email(newsletter, newscontent, abo_newsletter)
     @newsletter = newsletter
     @newscontent = newscontent
-    @abo_newsletters.each do |abo_newsletter|
-      if(abo_newsletter.enable == true)
-        @email = abo_newsletter.eMail
-        @id = abo_newsletter.id
+    @abo_newsletter = abo_newsletter
+        @email = @abo_newsletter.eMail
+        @id = @abo_newsletter.id
         @url = "http://vkm.marianit.de/abo_newsletter/" + String(@id)
         mail :to=> @email,
              :subject=> @newscontent.title
-      end
-    end
 
-      @news = NewsContent.find(@newscontent.id)
-      @news.sendStatus = true
-      @news.save
+
+
   end
 end
