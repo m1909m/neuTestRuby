cc7dcadbd9fd0d6a4a3699fc76c2adb557630ce3
Marian Vennewald <vennewald@axxessio.com>
2017-05-02 00:37:19 +0200

    add jquery definichen

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -7,9 +7,10 @@ class TinymceAssetsController < ApplicationController
     @image.file = image_params[:file]
     @image.save
   #  puts(@image.file_url)
-#    render :html => "<script>top.$('.mce-btn.mce-open').parent().find('.mce-textbox').val('" + @image.file_url + "');</script>".html_safe
+  #  render :html => "<script>alert('Hello Rails');</script>".html_safe
+   # render :html => "<script>top.$('.mce-btn.mce-open').parent().find('.mce-textbox').val('" + @image.file_url + "');</script>".html_safe
 
-    render :html => "<script>alert('test');$('#mceu_59-inp').val('" + @image.file_url + "');</script>".html_safe
+    render :html => "<script>$(document).ready(function(){ alert('test'); $('#mceu_59-inp').val('" + @image.file_url + "');});</script>".html_safe
    # render json: {
    #     location: @image.file_url
    # }, layout: false, content_type: "text/html"
