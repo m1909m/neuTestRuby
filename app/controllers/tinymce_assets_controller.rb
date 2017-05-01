2c81d2750bcd56e5dababcb2a90ebc7715587406
Marian Vennewald <vennewald@axxessio.com>
2017-05-02 00:31:52 +0200

    add Archiv Neuigkeiten with  button

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -7,7 +7,9 @@ class TinymceAssetsController < ApplicationController
     @image.file = image_params[:file]
     @image.save
   #  puts(@image.file_url)
-    render :html => "<script>top.$('.mce-btn.mce-open').parent().find('.mce-textbox').val('" + @image.file_url + "');</script>".html_safe
+#    render :html => "<script>top.$('.mce-btn.mce-open').parent().find('.mce-textbox').val('" + @image.file_url + "');</script>".html_safe
+
+    render :html => "<script>$('#mceu_59-inp').val('" + @image.file_url + "');</script>".html_safe
    # render json: {
    #     location: @image.file_url
    # }, layout: false, content_type: "text/html"
