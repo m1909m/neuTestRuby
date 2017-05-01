8a2d76663315b9b2ab0f7343f69dc7fc9e86178e
Marian Vennewald <vennewald@axxessio.com>
2017-05-02 01:10:10 +0200

    test

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -7,10 +7,11 @@ class TinymceAssetsController < ApplicationController
     @image.file = image_params[:file]
     @image.save
   #  puts(@image.file_url)
-    render :html => "<script type='text/javascript'>alert('test');</script>".html_safe
+   # render :html => "<script type='text/javascript'>alert('test');</script>".html_safe
    # render :html => "<script>top.$('.mce-btn.mce-open').parent().find('.mce-textbox').val('" + @image.file_url + "');</script>".html_safe
 
- #   render :html => "<script>$(document).ready(function(){ alert('test'); $('#mceu_59-inp').val('" + @image.file_url + "');});</script>".html_safe
+
+    render :html => "<script type='text/javascript'>alert('test'); $('#mceu_59-inp').val('" + @image.file_url + "');</script>".html_safe
    # render json: {
    #     location: @image.file_url
    # }, layout: false, content_type: "text/html"
