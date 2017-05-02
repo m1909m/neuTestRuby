1cf614dc7de1674fbcf3ce3e6a2f384a31042885
Marian Vennewald <vennewald@axxessio.com>
2017-05-02 11:38:10 +0200

    add

--- app/assets/javascripts/shops.js
+++ app/assets/javascripts/shops.js
@@ -331,7 +331,35 @@ shopApp.controller('calendarController', ['$scope', 'Cart', '$http', '$interval'
     $scope.eventSources = [$scope.events];
 
 }]);
+shopApp.directive('datetimepicker', function(){
+    return {
+        require: '?ngModel',
+        restrict: 'A',
+        link: function(scope, element, attrs, ngModel){
 
+            if(!ngModel) return; // do nothing if no ng-model
+
+            ngModel.$render = function(){
+                element.find('input').val( ngModel.$viewValue || '' );
+            };
+
+            element.datetimepicker({
+                language: 'de'
+            });
+
+            element.on('dp.change', function(){
+                scope.$apply(read);
+            });
+
+            read();
+
+            function read() {
+                var value = element.find('input').val();
+                ngModel.$setViewValue(value);
+            }
+        }
+    }
+});
 shopApp.controller('eventController', function($scope, Cart) {
 
     $scope.events = [];
