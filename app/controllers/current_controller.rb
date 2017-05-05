729202adb2ff97303c77ea9c623c44ac341a4299
Marian Vennewald <vennewald@axxessio.com>
2017-05-05 07:56:18 +0200

    add

--- app/controllers/current_controller.rb
+++ app/controllers/current_controller.rb
@@ -1,5 +1,13 @@
 class CurrentController < ApplicationController
   def index
+    if current_user.has_role?(:event)
+      @email = current_user.email
+      @accounts = Account.where(accountName: @email)
+      @resumes = []
+      @accounts.each do |a|
+        @resumes.push(Resume.find(a.resume_id))
+      end
+    end
   end
 
   def mitgliedschaft
