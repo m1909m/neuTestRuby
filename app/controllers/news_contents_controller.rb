d7ac042e25d2da991c849c30260fecd3a22bd07e
Marian Vennewald <vennewald@axxessio.com>
2017-05-17 16:14:00 +0200

    add

--- app/controllers/news_contents_controller.rb
+++ app/controllers/news_contents_controller.rb
@@ -44,7 +44,7 @@ class NewsContentsController < ApplicationController
       end
       puts(@aboNewsletters)
       I18n.locale = :de
-      SendEmailJob.set(wait: 20.seconds).perform_later(@layout, @news_content,@mails)
+      SendEmailJob.set(wait_until: @news_content.sendtime).perform_later(@layout, @news_content,@mails)
       respond_to do |f|
         f.html { redirect_to news_contents_url, notice: 'News wurde erfolgreich erstellt'}
       end
