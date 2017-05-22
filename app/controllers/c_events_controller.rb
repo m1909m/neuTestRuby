edccb3c69b82697e8ce3f60a240d46f6d58fb418
Marian Vennewald <vennewald@axxessio.com>
2017-05-22 19:16:50 +0200

    add

--- app/controllers/c_events_controller.rb
+++ app/controllers/c_events_controller.rb
@@ -102,7 +102,10 @@ class CEventsController < ApplicationController
       if current_user.email == "admin@vkm.de"
         puts("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
         @crypt = ActiveSupport::MessageEncryptor.new("<\xE4i\x8BB\xD1[\x98{\x9F\xDE1\xC6u\x06\xFC\xF8!\xC6\xED\xFEh\xAF\xF5\xA2\xE8\xC6jy,\x19/1\xBB\xEE\x9Es\xA3(\x971\xD3\xBFR!8\x13\xB8\xCDV\xD7\x03\xC8R@v\xF5.\xCF\xBAA\x890\xD8")
-        ContactMailer.create_event("verbandkirchlichermmitarbeiter@gmail.com", @crypt.decrypt_and_verify(@user.pass), @c_event)
+        puts(@crypt)
+        @pass = @crypt.decrypt_and_verify(@user.pass)
+        puts(@pass)
+        ContactMailer.create_event("verbandkirchlichermmitarbeiter@gmail.com", @pass, @c_event)
         flash[:success] = "Sie haben erfolgreich die Veranstaltung erstellt. Sie erhalten in kürze eine E-Mail."
       else
         puts("------------------------------------------------------------------------------------------------------------------------------")
