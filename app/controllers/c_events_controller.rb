ef2057eec1a173ab74b0fb2b1e9b7b04743e9ff5
Marian Vennewald <vennewald@axxessio.com>
2017-06-29 00:08:37 +0200

    add

--- app/controllers/c_events_controller.rb
+++ app/controllers/c_events_controller.rb
@@ -99,7 +99,7 @@ class CEventsController < ApplicationController
 
     if @c_event.save
       puts(current_user.email)
-      if current_user.email == "admin@vkm.de"
+      if current_user.email == "admin@vkm.de" || current_user.email == "admin"
         @crypt = ActiveSupport::MessageEncryptor.new("<\xE4i\x8BB\xD1[\x98{\x9F\xDE1\xC6u\x06\xFC\xF8!\xC6\xED\xFEh\xAF\xF5\xA2\xE8\xC6jy,\x19/1\xBB\xEE\x9Es\xA3(\x971\xD3\xBFR!8\x13\xB8\xCDV\xD7\x03\xC8R@v\xF5.\xCF\xBAA\x890\xD8")
         @pass = @crypt.decrypt_and_verify(@user.pass)
 
