19d0dc8bd6237b87e46c8c31fd535cdbf51d0bbb
Marian Vennewald <vennewald@axxessio.com>
2017-07-10 15:46:30 +0200

    add

--- app/controllers/c_events_controller.rb
+++ app/controllers/c_events_controller.rb
@@ -54,7 +54,10 @@ class CEventsController < ApplicationController
     @member.sendAccept = true
     @event = CEvent.find(@member.event_id)
     @room = Room.find(@event.room_id)
+    puts(@room)
     @user = User.where( email: @event.accountName)
+    puts(@user)
+    puts(@user.pass)
     @crypt = ActiveSupport::MessageEncryptor.new("<\xE4i\x8BB\xD1[\x98{\x9F\xDE1\xC6u\x06\xFC\xF8!\xC6\xED\xFEh\xAF\xF5\xA2\xE8\xC6jy,\x19/1\xBB\xEE\x9Es\xA3(\x971\xD3\xBFR!8\x13\xB8\xCDV\xD7\x03\xC8R@v\xF5.\xCF\xBAA\x890\xD8")
     @pass = @crypt.decrypt_and_verify(@user.pass)
     ContactMailer.accept_event(@member, @event, @room, @user, @pass).deliver
