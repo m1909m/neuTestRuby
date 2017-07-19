778e4448b3971e2f2640fc3b8298d5fe210127a8
Marian Vennewald <vennewald@axxessio.com>
2017-07-19 18:20:22 +0200

    add

--- app/controllers/abo_newsletters_controller.rb
+++ app/controllers/abo_newsletters_controller.rb
@@ -16,7 +16,7 @@ class AboNewslettersController < ApplicationController
   # GET /abo_newsletters/new
   def new
     @abo_newsletter = AboNewsletter.new
-    @newsletter = NewsContent.all.order(:sendtime)
+    @newsletter = NewsContent.all.order(sendtime: :desc)
   end
 
   # GET /abo_newsletters/1/edit
