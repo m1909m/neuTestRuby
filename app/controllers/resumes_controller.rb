19a9431e54eb567d32c2103900e7dabc6a381bed
Marian Vennewald <vennewald@axxessio.com>
2017-08-22 13:45:09 +0200

    add

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -111,8 +111,9 @@ class ResumesController < ApplicationController
 
   def changePasswordL
     @user = User.find(params[:id])
+
     @crypt = ActiveSupport::MessageEncryptor.new("<\xE4i\x8BB\xD1[\x98{\x9F\xDE1\xC6u\x06\xFC\xF8!\xC6\xED\xFEh\xAF\xF5\xA2\xE8\xC6jy,\x19/1\xBB\xEE\x9Es\xA3(\x971\xD3\xBFR!8\x13\xB8\xCDV\xD7\x03\xC8R@v\xF5.\xCF\xBAA\x890\xD8")
-    @pass = @crypt.decrypt_and_verify(@user.passwordEncrypt)
+    @pass = @crypt.decrypt_and_verify(@user.pass)
     if @pass == user_change_params[:oldpassword]
       if user_change_params[:newpassword] == user_change_params[:newpassword_confirm]
         @user.update_attributes(:password => user_change_params[:newpassword], :password_confirmation => user_change_params[:newpassword_confirm])
