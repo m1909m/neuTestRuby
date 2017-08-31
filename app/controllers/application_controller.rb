ce834393afeee87629d6ba016ebfcd736111d216
Marian Vennewald <vennewald@axxessio.com>
2017-08-31 13:14:26 +0200

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || "/zugangsbereich"
   end
 
 
